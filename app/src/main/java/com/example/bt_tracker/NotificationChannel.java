02a12385462a4397db74983f48cfdcbf351fe7c9
RinaYamashita <2020m134@kuas.ac.jp>
2020-06-05 11:54:13 +0900

    Start from class5->task5->step5

---
+++ app/src/main/java/com/example/bt_tracker/NotificationChannel.java
@@ -0,0 +1,6 @@
+package com.example.bt_tracker;
+
+class NotificationChannel {
+    public NotificationChannel(String channelID, String channelName, int importance) {
+    }
+}
---
+++ app/src/main/java/com/example/bt_tracker/ReminderBroadcastReceiver.java
@@ -0,0 +1,4 @@
+package com.example.bt_tracker;
+
+class ReminderBroadcastReceiver {
+}
