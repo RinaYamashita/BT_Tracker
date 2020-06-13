48ed7f7978be84da8df2741fb958415e9a62c02c
RinaYamashita <2020m134@kuas.ac.jp>
2020-06-13 13:36:19 +0900

    Homework 5.3: extended ReminderBroadcastReceiver to start LogActivity directly from notification

--- app/src/main/java/com/example/bt_tracker/MainActivity.java
+++ app/src/main/java/com/example/bt_tracker/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.bt_tracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlarmManager;
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -14,6 +11,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -44,44 +43,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.MainButton1:
-                //If MainButton1 is clicked, do something Intent toLog = new Intent(this,LogActivity.class);
-                //    startActivity(toLog);
+                //If MainButton1 is clicked, do something
+                Intent toLog = new Intent(this, LogActivity.class);
+                startActivity(toLog);
                 break;
             case R.id.MainButton2:
-                //If MainButton2 is clicked, do something Intent toNormal = new Intent(this,NormalActivity.class);
-                //        startActivity(toNormal);
+                //If MainButton2 is clicked, do something
+                Intent toNormal = new Intent(this, NormalActivity.class);
+                startActivity(toNormal);
                 break;
             case R.id.MainButton3:
-                //If MainButton3 is clicked, do something Intent toMechanism = new Intent(this,MechanismActivity.class);
-                //        startActivity(toMechanism);
+                //If MainButton3 is clicked, do something
+                Intent toMechanism = new Intent(this, MechanismActivity.class);
+                startActivity(toMechanism);
                 break;
             case R.id.MainButton4:
-                //If MainButton4 is clicked, do something Intent toLink2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.healthline.com/health/how-to-break-a-fever"));
-                //        startActivity(toLink2);
-                break;
-            case R.id.setReminderButton:
-                //If MainButton4 is clicked, do something Intent intent = new Intent(this, ReminderBroadcastReceiver.class);
-                //        //Create a pending intent so that the intent object above will only fire when alarm triggers
-                //        PendingIntent pd = PendingIntent.getBroadcast(this, 0, intent, 0);
-                //        //Create an AlarmManager
-                //        AlarmManager alarmManager;
-                //        alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
-                //        //Repeating interval for the alarmManager is set to 6 second for demonstration purpose
-                //        //In real world application, users may want to get daily reminder
-                //        //In that case, set the interval to 1000 * 60 * 60 * 24
-                //        long interval = 1000*6;
-                //        //set up a repeating alarm so that the notification reminder gets fired at the set interval
-                //        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,System.currentTimeMillis(), interval, pd);
+                //If MainButton4 is clicked, do something
+                Intent toLink2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.healthline.com/health/how-to-break-a-fever"));
+                startActivity(toLink2);
                 break;
         }
     }
 
-
     public void setReminder(View view) {
         // When user clicks "SET REMINDER" button, a toast message will pop up to let user know that a reminder is set
         Toast.makeText(this, "Reminder set!", Toast.LENGTH_SHORT).show();
+
         //Create an intent object to start the ReminderBroadcastReceiver class
-    }
+                Intent intent = new Intent(this, ReminderBroadcastReceiver.class);
+
+                        //Create a pending intent so that the intent object above will only fire when alarm rings
+                PendingIntent pd = PendingIntent.getBroadcast(this, 0, intent, 0);
+
+                       //Create an AlarmManager
+                AlarmManager alarmManager= (AlarmManager)
+                        getSystemService(ALARM_SERVICE);
+
+                //Repeating interval for the alarmManager is set to 6 second for demonstration purpose
+                //In real world application, users may want to get daily reminder
+                //In that case, set the interval to 1000 * 60 * 60 * 24
+                long interval = 1000*6;
+                //set up a repeating alarm so that the notification reminder gets fired at the set interval
+                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,System.currentTimeMillis(), interval, pd);
+        }
+
 
     private void createNotificationchannel(){
         //First, check SDK version
--- app/src/main/java/com/example/bt_tracker/ReminderBroadcastReceiver.java
+++ app/src/main/java/com/example/bt_tracker/ReminderBroadcastReceiver.java
@@ -1,9 +1,14 @@
 package com.example.bt_tracker;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
@@ -18,7 +23,26 @@ public class ReminderBroadcastReceiver extends BroadcastReceiver {
                 .setContentTitle("Notification from BT Tracker")
                 .setContentText("Please log your body temperature now")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-        notificationManager.notify(200,builder.build()); // ID could be any number
+        notificationManager.notify(200, builder.build()); // ID could be any number
+
+        Intent notificationIntent  = new Intent(context,LogActivity.class);
+        PendingIntent contentIntent =PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Resources res = context.getResources();
+        NotificationCompat.Builder builder1 = new NotificationCompat.Builder(context);
+        builder1.setContentIntent(contentIntent)
+                .setTicker("Please log your body temperature now")
+                .setWhen(System.currentTimeMillis())
+                .setAutoCancel(true);
+        Notification n = builder.getNotification();
+
+        n.defaults = Notification.DEFAULT_ALL;
+        nm.notify(0, n);
     }
 }
+
